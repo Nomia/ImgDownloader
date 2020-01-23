904fa32e4ec45ccfa0843acacfe9ca9af8eeeae3
saisreeharsha <saisriharsha.dubagunta@gmail.com>
2020-01-23 15:28:32 +0530

    Fix for Issue #6
    Fixed "Download photo from url says successful even when no permissions to access photos (and it fails) #6"

--- src/ios/CDVImgDownloader.m
+++ src/ios/CDVImgDownloader.m
@@ -3,6 +3,7 @@
 #import <Cordova/CDVPlugin.h>
 #import <Cordova/CDVPluginResult.h>
 #import "CDVImgDownloader.h"
+#import <Photos/Photos.h>
 
 @implementation CDVImgDownloader
 
@@ -26,30 +27,54 @@
 	}];
 }
 
-- (void)downloadWithUrl:(CDVInvokedUrlCommand*)command {
-	CDVPluginResult *pluginResult = nil;
-    NSURL *url = [NSURL URLWithString:[command.arguments objectAtIndex:0]];
-	NSData *args = [NSData dataWithContentsOfURL:url];
+- (void) downloadWithUrl: (CDVInvokedUrlCommand * ) command {
+    CDVPluginResult * pluginResult = nil;
+    NSURL * url = [NSURL URLWithString: [command.arguments objectAtIndex: 0]];
+    NSData * args = [NSData dataWithContentsOfURL: url];
 
-	if (args != nil && [args length] > 0) {
-		 @try
-        {
-            UIImage *image = [UIImage imageWithData:args];
-            NSData *imgdata = UIImagePNGRepresentation(image);
-            UIImage *image2 = [UIImage imageWithData:imgdata];
-            UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    if (args != nil && [args length] > 0) {
+      @try {
+        PHAuthorizationStatus status = [PHPhotoLibrary authorizationStatus];
 
+        if (status == PHAuthorizationStatusAuthorized) {
+          // Access has been granted.
+          [self saveImageToPhotos: pluginResult command: command data: args];
+        } else if (status == PHAuthorizationStatusDenied) {
+          // Access has been denied.
+        } else if (status == PHAuthorizationStatusNotDetermined) {
 
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"success"];
-            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+          // Access has not been determined.
+          [PHPhotoLibrary requestAuthorization: ^ (PHAuthorizationStatus status) {
+
+            if (status == PHAuthorizationStatusAuthorized) {
+              // Access has been granted.
+              [self saveImageToPhotos: pluginResult command: command data: args];
+            } else {
+              // Access has been denied.
+            }
+          }];
         }
-        @catch (NSException *exception) {
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        }
-	}
 
-}
+      }
+      @catch(NSException * exception) {
+        pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR];
+        [self.commandDelegate sendPluginResult: pluginResult callbackId: command.callbackId];
+      }
+    }
+
+  }
+
+  - (void) saveImageToPhotos: (CDVPluginResult) pluginRes command: (CDVInvokedUrlCommand * ) command data: (NSData) data {
+
+    UIImage * image = [UIImage imageWithData: data];
+    // NSData *imgdata = UIImagePNGRepresentation(image);
+    // UIImage *image2 = [UIImage imageWithData:imgdata];
+    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image: didFinishSavingWithError: contextInfo: ), nil);
+
+    pluginRes = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString: @ "success"];
+    [self.commandDelegate sendPluginResult: pluginRes callbackId: command.callbackId];
+
+  }
 
 - (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error
   contextInfo:(void *)contextInfo
